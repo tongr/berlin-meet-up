c80afd483eee625238d27436adb438dc342479f1
tongr <tongr@online>
2012-09-13 19:28:09 +0200

    comments added

--- js/api_keys.js
+++ js/api_keys.js
@@ -1,9 +1,11 @@
+// this file has to be renamed to api_keys.private.js
 (function(window) {
   var APIKeys = this;
+  // TODO please add your private API keys here
   this.Google = 'PLEASE ENTER THE GOOGLE API KEY HERE!';
 
   // Map over other "APIKeys" object
   _APIKeys = window.APIKeys;
   // expose the key store
   window.APIKeys = APIKeys;
-})(window); 
+})(window);
