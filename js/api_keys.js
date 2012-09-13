0240c8b2fdab6c52fb0e12b7444671da1b6f43df
tongr <tongr@online>
2012-09-13 19:20:16 +0200

    added main page with initialized berlin map

--- js/api_keys.js
+++ js/api_keys.js
@@ -1,3 +1,9 @@
-function APIKeys() {
-	this.Google = 'PLEASE ENTER THE GOOGLE API KEY HERE!';
-}
+(function(window) {
+  var APIKeys = this;
+  this.Google = 'PLEASE ENTER THE GOOGLE API KEY HERE!';
+
+  // Map over other "APIKeys" object
+  _APIKeys = window.APIKeys;
+  // expose the key store
+  window.APIKeys = APIKeys;
+})(window); 
